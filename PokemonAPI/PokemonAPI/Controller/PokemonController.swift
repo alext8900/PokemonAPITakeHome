85e8cb626e7113ab419fb643be376ca000099258
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2021-04-17 18:28:04 -0500

    Added pokemon Fetch Request and image request
    Added my pokemon network controller. Added fetch request and fetch image request for detail view

---
+++ PokemonAPI/PokemonAPI/Controller/PokemonController.swift
@@ -0,0 +1,81 @@
+//
+//  PokemonController.swift
+//  PokemonAPI
+//
+//  Created by Alex Thompson on 4/17/21.
+//
+
+import Foundation
+import UIKit
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
+
+enum NetworkError: Error {
+    case otherError
+    case badData
+    case noDecode
+    case noError
+}
+
+class PokemonController {
+    var pokemonList: [Pokemon] = []
+    var pokemonImages: [URL] = []
+    let baseURL = URL(string: "https://pokeapi.co/api/v2/pokemon")
+    func fetchPokemon(pokemonName: String, completion: @escaping ((Result<Pokemon, Error>) -> Void)) {
+        let requestURL = baseURL?.appendingPathComponent(pokemonName.lowercased())
+        guard let url = requestURL else { return }
+        var request = URLRequest(url: url)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { data, _, error in
+            if let error = error {
+                print("Error fetching data: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data returned: \(error?.localizedDescription)")
+                return
+            }
+            
+            do {
+                let pokemonSearch = try JSONDecoder().decode(Pokemon.self, from: data)
+                completion(.success(pokemonSearch))
+            } catch {
+                print("Unable to decode data into object of type [Pokemon]: \(error.localizedDescription)")
+            }
+        }.resume()
+    }
+    
+    func fetchImage(from imageURL: String, completion: @escaping(UIImage?) -> Void) {
+        guard let imageURL = URL(string: imageURL) else {
+            completion(nil)
+            return }
+        
+        var request = URLRequest(url: imageURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { imageData, _, error in
+            if let error = error {
+                print("Error fetching image: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let data = imageData else {
+                print("No data provided for image: \(imageURL)")
+                completion(nil)
+                return
+            }
+            
+            let image = UIImage(data: data)
+            
+            completion(image)
+        }.resume()
+    }
+    
+    func addPokemon(pokemon: Pokemon) {
+        pokemonList.append(pokemon)
+    }
+}
