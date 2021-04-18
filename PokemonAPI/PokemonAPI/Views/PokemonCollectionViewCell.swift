c46c1f831b09091b1dd3a54494b73980e7aaa20a
Alexander Thompson <53663334+alext8900@users.noreply.github.com>
2021-04-17 19:50:04 -0500

    Completed Collection View Controller
    I an currently search pokemon and get a price on the pokemon based on the calculation given in the instructions. Cant currrently save pokemon to collection. view

---
+++ PokemonAPI/PokemonAPI/Views/PokemonCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  PokemonCollectionViewCell.swift
+//  PokemonAPI
+//
+//  Created by Alex Thompson on 4/17/21.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var nameLabel: UILabel!
+}
--- PokemonAPI/PokemonAPI/Views/PokemonCollectionViewController.swift
+++ PokemonAPI/PokemonAPI/Views/PokemonCollectionViewController.swift
@@ -25,35 +25,44 @@ class PokemonCollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using [segue destinationViewController].
-        // Pass the selected object to the new view controller.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        collectionView.reloadData()
     }
-    */
+
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return 0
+        return pokemonController.pokemonList.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-    
-        // Configure the cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! PokemonCollectionViewCell
+        let pokemonMonster = pokemonController.pokemonList[indexPath.item]
+        cell.nameLabel.text = pokemonMonster.name
     
         return cell
+    }
+    
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "SelectSegue" {
+            guard let pokemonVC = segue.destination as? PokemonPurchaseViewController,
+                  let indexPath = collectionView.indexPathsForSelectedItems?.first else { return }
+            pokemonVC.pokemonController = pokemonController
+            pokemonVC.pokemon = pokemonController.pokemonList[indexPath.item]
+            
+        } else if segue.identifier == "SearchSegue" {
+            if let searchVC = segue.destination as? PokemonPurchaseViewController {
+                searchVC.pokemonController = pokemonController
+            }
+        }
     }
 }
